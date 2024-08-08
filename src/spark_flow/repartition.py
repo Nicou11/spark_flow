6f884462ed9b84eceebcf75bd15c831ca8aafe19
Nicou11 <lyoungin1204@gmail.com>
2024-08-08 14:40:21 +0900

    asd

--- src/spark_flow/repartition.py
+++ src/spark_flow/repartition.py
@@ -5,7 +5,7 @@ def re_partition(load_dt, from_path='tmp/sparkdata'):
     home_dir = os.path.expanduser("~")
     read_path = f'{home_dir}/{from_path}/load_dt={load_dt}'
     write_base = f'{home_dir}/data/movie/repartition'
-    write_path = f'{write_dir}/load_dt={load_dt}'
+    write_path = f'{write_base}/load_dt={load_dt}'
 
     df = pd.read_parquet(read_path)
     df['load_df'] = load_dt
